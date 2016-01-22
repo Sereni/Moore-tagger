a7c1ed2acd7868bccf03f92fb4da2f0fd0c216e8
Elmira <cclikespizza@gmail.com>
2016-01-22 16:09:51 +0300

    added docstrings >_<

--- hse_crawler.ru.py
+++ hse_crawler.ru.py
@@ -6,6 +6,10 @@ import os
 
 
 def get_links(dirname):
+    '''
+    :param dirname: папка с хтмл файлами, где хранятся страницы типа http://www.hse.ru/news/page5.html (списки статей с краткими аннотациями и ссылками на полные версии)
+    :return: links - массив всех ссылок на полные версии статей (только те, которые на портале hse.ru, перенаправления на другие порталы исключаются)
+    '''
     links = []
     for fname in os.listdir(dirname):
         with open(fname, 'r', encoding='utf-8') as f:
@@ -17,12 +21,15 @@ def get_links(dirname):
 
 
 def get_text(fname):
+    '''
+    :param fname: путь к хтмл файлу с полной версией новостной статьи
+    :return: текст статьи (строка)
+    '''
     with open(fname, 'r', encoding='utf-8') as f:
         html = f.read()
     root = lxml.html.fromstring(html)
-    posts = root.xpath(u'//*[contains(@class, "post-title")]/text()')
+    posts = root.xpath(u'//*[contains(@class, "post-title")]/text()') + ['\n']
     posts += root.xpath(u'//*[contains(@class, "post__text")]/p/text()')
-    return posts
+    return ' '.join(posts)
 
-for i in get_text('news.html'):
-    print(i)
+print (get_text('news.html'))
