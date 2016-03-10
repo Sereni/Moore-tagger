9615350c9fa922ac7b805489328939371617e84e
liza <lizaku77@gmail.com>
2016-03-10 16:35:16 +0300

    ROC. Doesnt't work yet

--- classifier.py
+++ classifier.py
@@ -1,4 +1,8 @@
 __author__ = 'Sereni'
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import label_binarize
 from sklearn.linear_model import SGDClassifier
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report
@@ -6,6 +10,7 @@ from sklearn.externals import joblib
 from sklearn import grid_search
 import csv
 import numpy
+from scipy import interp
 
 
 def import_csv(path):
@@ -65,19 +70,7 @@ def import_as_dict(path):
     return data_matrix, target_matrix
 
 
-if __name__ == '__main__':
-    import warnings
-    warnings.filterwarnings("ignore")
-
-    # import data
-    print("Import data")
-    data, target = import_as_dict('feature_matrix_clusters.csv')
-    # todo early stopping
-
-    # split data into train and test subsets
-    print("Split data")
-    data_train, data_test, target_train, target_test = train_test_split(data, target)
-
+def greedy():
     # Set the parameters by cross-validation
     tuned_parameters = [{'loss': ['hinge', 'log'], 'shuffle': [True],
                          'learning_rate': ['constant'], 'eta0': [2**(-8)], 'average': [True, False],
@@ -117,3 +110,97 @@ if __name__ == '__main__':
 
         joblib.dump(best, 'model2.pkl')
         print()
+
+
+if __name__ == '__main__':
+    import warnings
+    warnings.filterwarnings("ignore")
+
+    # import data
+    print("Import data")
+    data, target = import_as_dict('feature_matrix_clusters.csv')
+    # todo early stopping
+
+    # split data into train and test subsets
+    print("Split data")
+    data_train, data_test, target_train, target_test = train_test_split(data, target)
+    y = label_binarize(target, classes=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18])
+    n_classes = y.shape[1]
+    # greedy()
+
+    clf = SGDClassifier(penalty='elasticnet', eta0=0.00390625, learning_rate='constant', alpha=1e-06, loss='hinge')
+    y_score = clf.fit(data_train, target_train).decision_function(data_test)
+    print(y_score)
+
+    print('Going ROCs!')
+    # Compute ROC curve and ROC area for each class
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(target_test[:, i], y_score[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(target_test.ravel(), y_score.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+
+    ##############################################################################
+    # Plot of a ROC curve for a specific class
+    plt.figure()
+    plt.plot(fpr[2], tpr[2], label='ROC curve (area = %0.2f)' % roc_auc[2])
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+    ##############################################################################
+    # Plot ROC curves for the multiclass problem
+
+    # Compute macro-average ROC curve and ROC area
+
+    # First aggregate all false positive rates
+    all_fpr = numpy.unique(numpy.concatenate([fpr[i] for i in range(n_classes)]))
+
+    # Then interpolate all ROC curves at this points
+    mean_tpr = numpy.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    # Plot all ROC curves
+    plt.figure()
+    plt.plot(fpr["micro"], tpr["micro"],
+             label='micro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["micro"]),
+             linewidth=2)
+
+    plt.plot(fpr["macro"], tpr["macro"],
+             label='macro-average ROC curve (area = {0:0.2f})'
+                   ''.format(roc_auc["macro"]),
+             linewidth=2)
+
+    for i in range(n_classes):
+        plt.plot(fpr[i], tpr[i], label='ROC curve of class {0} (area = {1:0.2f})'
+                                       ''.format(i, roc_auc[i]))
+
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Some extension of Receiver operating characteristic to multi-class')
+    plt.legend(loc="lower right")
+    plt.show()
